1348e45c2b911c67d96a851478696802880c6ddc
kuan ting <kusakawazeusu@gmail.com>
2025-07-04 09:52:28 +0800

    fix: image onload

--- src/components/ResultImage.tsx
+++ src/components/ResultImage.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 function ResultImage({ score, rank }: { score: number; rank: number }) {
     const imageIndex = getImageIndex(score);
     const [startCounting, setStartCounting] = useState(false);
+    const [isImageLoaded, setImageLoaded] = useState(false);
 
     useEffect(() => {
         setTimeout(() => setStartCounting(true), 1000);
@@ -32,73 +33,78 @@ function ResultImage({ score, rank }: { score: number; rank: number }) {
                 height={570}
                 width={320}
                 alt="main image"
+                onLoad={() => setImageLoaded(true)}
             />
 
-            <motion.div
-                className={styles.characterImageContainer}
-                style={{ ...characterPosition }}
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{ type: "spring" }}
-            >
-                <img
-                    src={`/assets/result/ch${imageIndex}.png`}
-                    className={styles.image}
-                />
-
-                {accessories[imageIndex].map((accessory, index) => (
-                    <motion.img
-                        animate={{
-                            scale: [1, 1.2, 1],
-                        }}
-                        transition={{
-                            duration: 1,
-                            repeat: Infinity,
-                            ease: "easeInOut",
-                            delay: 0.1 * index,
-                        }}
-                        key={`accessory-${index}`}
-                        src={`/assets/${accessory.type}.png`}
-                        style={{
-                            position: "absolute",
-                            left: accessory.x,
-                            top: accessory.y,
-                            width: accessory.size,
-                        }}
-                    />
-                ))}
-            </motion.div>
-
-            <motion.div className={styles.scoreContainer}>
-                {startCounting ? <Counter value={score} /> : 0}
-            </motion.div>
-
-            <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ delay: 0.4 }}
-                viewport={{ once: true }}
-                className={styles.rank}
-            >
-                {rank}
-            </motion.div>
-
-            <div className={styles.textContainer}>
-                {texts[imageIndex].split("\n").map((t, index) => (
-                    <motion.p
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{
-                            delay: 0.4 + index * 0.1,
-                            duration: 0.5,
-                            ease: "linear",
-                        }}
-                        key={t}
+            {isImageLoaded ? (
+                <>
+                    <motion.div
+                        className={styles.characterImageContainer}
+                        style={{ ...characterPosition }}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{ type: "spring" }}
                     >
-                        {t}
-                    </motion.p>
-                ))}
-            </div>
+                        <img
+                            src={`/assets/result/ch${imageIndex}.png`}
+                            className={styles.image}
+                        />
+
+                        {accessories[imageIndex].map((accessory, index) => (
+                            <motion.img
+                                animate={{
+                                    scale: [1, 1.2, 1],
+                                }}
+                                transition={{
+                                    duration: 1,
+                                    repeat: Infinity,
+                                    ease: "easeInOut",
+                                    delay: 0.1 * index,
+                                }}
+                                key={`accessory-${index}`}
+                                src={`/assets/${accessory.type}.png`}
+                                style={{
+                                    position: "absolute",
+                                    left: accessory.x,
+                                    top: accessory.y,
+                                    width: accessory.size,
+                                }}
+                            />
+                        ))}
+                    </motion.div>
+
+                    <motion.div className={styles.scoreContainer}>
+                        {startCounting ? <Counter value={score} /> : 0}
+                    </motion.div>
+
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        transition={{ delay: 0.4 }}
+                        viewport={{ once: true }}
+                        className={styles.rank}
+                    >
+                        {rank}
+                    </motion.div>
+
+                    <div className={styles.textContainer}>
+                        {texts[imageIndex].split("\n").map((t, index) => (
+                            <motion.p
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    delay: 0.4 + index * 0.1,
+                                    duration: 0.5,
+                                    ease: "linear",
+                                }}
+                                key={t}
+                            >
+                                {t}
+                            </motion.p>
+                        ))}
+                    </div>
+                </>
+            ) : null}
         </div>
     );
 }

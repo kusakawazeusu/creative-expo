47a1e75ac3165c52ccf73bfc6b5ca61d5fb4d94f
kuan ting <kusakawazeusu@gmail.com>
2025-07-28 10:47:46 +0800

    fix: 修改分數區間

--- src/utils/drawImage.ts
+++ src/utils/drawImage.ts
@@ -59,16 +59,16 @@ export const accessories: ACCESSORY[][] = [
 
 export function getImageIndex(score: number) {
     let imageIndex = 1;
-    if (score > 100 && score < 201) {
+    if (score > 300 && score <= 500) {
         imageIndex = 2;
     }
-    if (score > 200 && score < 301) {
+    if (score > 500 && score <= 700) {
         imageIndex = 3;
     }
-    if (score > 300 && score < 401) {
+    if (score > 700 && score <= 1000) {
         imageIndex = 4;
     }
-    if (score > 400) {
+    if (score > 1000) {
         imageIndex = 5;
     }
 
